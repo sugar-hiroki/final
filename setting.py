ca95c32a3ac62ab9d39cd7ae5d0ab8ec9663c22e
sugar-hiroki <0gy082117885b4m@ezweb.ne.jp>
2019-02-06 22:07:42 +0900

    search.py

---
+++ setting.py
@@ -0,0 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+CK = os.environ.get("CONSUMER_KEY")
+CS = os.environ.get("CONSUMER_SECRET")
+AT = os.environ.get("ACCESS_TOKEN")
+ATS = os.environ.get("ACCESS_TOKEN_SECRET")
